753bffced82867af0ef9b3ecd2a588618a5e768f
McJawry <126582245+McJawry@users.noreply.github.com>
2026-06-13 02:43:42 +0200

    Rearrange files

--- Launch Hint Tracker - Mac.command
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-sh "$DIR/launch-hint-tracker - Linux.sh"
--- Launch Hint Tracker - Windows.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-setlocal
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0app\launch-local-server.ps1"
